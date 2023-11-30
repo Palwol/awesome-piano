0a9ed19326f84a500292540698febc7318f6ef81
palwol <mirim920@naver.com>
2023-12-01 01:52:26 +0900

    chore: styled-components 설치 및 displayName 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ plugins: [['@swc/plugin-styled-components', { displayName: true }]] })],
   server: {
     port: 3000,
   },
