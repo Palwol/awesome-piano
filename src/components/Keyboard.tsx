90a00b2d25cb0e41572c5d7a9339397f4903a8dc
mirim <mirim920@naver.com>
2025-06-08 21:11:15 +0900

    refactor: Keyboard 컴포넌트 Key 속성값 분리

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,54 +1,118 @@
 import styled from 'styled-components';
 import Key from './Key';
 
+interface KeyConfig {
+  name: string;
+  filePath: string;
+  keyCode: string;
+  isBlack?: boolean;
+  left?: number;
+}
+
+const whiteKeys: KeyConfig[] = [
+  { name: 'Z', filePath: '/assets/piano/C3.mp3', keyCode: 'KeyZ' },
+  { name: 'X', filePath: '/assets/piano/D3.mp3', keyCode: 'KeyX' },
+  { name: 'C', filePath: '/assets/piano/E3.mp3', keyCode: 'KeyC' },
+  { name: 'V', filePath: '/assets/piano/F3.mp3', keyCode: 'KeyV' },
+  { name: 'B', filePath: '/assets/piano/G3.mp3', keyCode: 'KeyB' },
+  { name: 'N', filePath: '/assets/piano/A3.mp3', keyCode: 'KeyN' },
+  { name: 'M', filePath: '/assets/piano/B3.mp3', keyCode: 'KeyM' },
+  { name: ',', filePath: '/assets/piano/C4.mp3', keyCode: 'Comma' },
+  { name: '.', filePath: '/assets/piano/D4.mp3', keyCode: 'Period' },
+  { name: '/', filePath: '/assets/piano/E4.mp3', keyCode: 'Slash' },
+];
+
+const blackKeys: KeyConfig[] = [
+  { name: 'S', filePath: '/assets/piano/Db3.mp3', keyCode: 'KeyS', isBlack: true, left: 30.5 },
+  { name: 'D', filePath: '/assets/piano/Eb3.mp3', keyCode: 'KeyD', isBlack: true, left: 87.5 },
+  { name: 'G', filePath: '/assets/piano/Gb3.mp3', keyCode: 'KeyG', isBlack: true, left: 201.5 },
+  { name: 'H', filePath: '/assets/piano/Ab3.mp3', keyCode: 'KeyH', isBlack: true, left: 258.5 },
+  { name: 'J', filePath: '/assets/piano/Bb3.mp3', keyCode: 'KeyJ', isBlack: true, left: 315.5 },
+  { name: 'L', filePath: '/assets/piano/Db4.mp3', keyCode: 'KeyL', isBlack: true, left: 429.5 },
+  { name: ';', filePath: '/assets/piano/Eb4.mp3', keyCode: 'Semicolon', isBlack: true, left: 486.5 },
+];
+
+const whiteKeys2: KeyConfig[] = [
+  { name: 'Q', filePath: '/assets/piano/F4.mp3', keyCode: 'KeyQ' },
+  { name: 'W', filePath: '/assets/piano/G4.mp3', keyCode: 'KeyW' },
+  { name: 'E', filePath: '/assets/piano/A4.mp3', keyCode: 'KeyE' },
+  { name: 'R', filePath: '/assets/piano/B4.mp3', keyCode: 'KeyR' },
+  { name: 'T', filePath: '/assets/piano/C5.mp3', keyCode: 'KeyT' },
+  { name: 'Y', filePath: '/assets/piano/D5.mp3', keyCode: 'KeyY' },
+  { name: 'U', filePath: '/assets/piano/E5.mp3', keyCode: 'KeyU' },
+  { name: 'I', filePath: '/assets/piano/F5.mp3', keyCode: 'KeyI' },
+  { name: 'O', filePath: '/assets/piano/G5.mp3', keyCode: 'KeyO' },
+  { name: 'P', filePath: '/assets/piano/A5.mp3', keyCode: 'KeyP' },
+  { name: '[', filePath: '/assets/piano/B5.mp3', keyCode: 'BracketLeft' },
+  { name: ']', filePath: '/assets/piano/C6.mp3', keyCode: 'BracketRight' },
+];
+
+const blackKeys2: KeyConfig[] = [
+  { name: '2', filePath: '/assets/piano/Gb4.mp3', keyCode: 'Digit2', isBlack: true, left: 30.5 },
+  { name: '3', filePath: '/assets/piano/Ab4.mp3', keyCode: 'Digit3', isBlack: true, left: 87.5 },
+  { name: '4', filePath: '/assets/piano/Bb4.mp3', keyCode: 'Digit4', isBlack: true, left: 144.5 },
+  { name: '6', filePath: '/assets/piano/Db5.mp3', keyCode: 'Digit6', isBlack: true, left: 258.5 },
+  { name: '7', filePath: '/assets/piano/Eb5.mp3', keyCode: 'Digit7', isBlack: true, left: 315.5 },
+  { name: '9', filePath: '/assets/piano/Gb5.mp3', keyCode: 'Digit9', isBlack: true, left: 429.5 },
+  { name: '0', filePath: '/assets/piano/Ab5.mp3', keyCode: 'Digit0', isBlack: true, left: 486.5 },
+  { name: '-', filePath: '/assets/piano/Bb5.mp3', keyCode: 'Minus', isBlack: true, left: 543.5 },
+];
+
 const Keyboard = () => {
   const audioCtx = new AudioContext();
 
   return (
     <Container>
       <Row>
-        <Key name="Q" filePath="/assets/piano/F4.mp3" audioCtx={audioCtx} keyCode="KeyQ" />
-        <Key name="W" filePath="/assets/piano/G4.mp3" audioCtx={audioCtx} keyCode="KeyW" />
-        <Key name="E" filePath="/assets/piano/A4.mp3" audioCtx={audioCtx} keyCode="KeyE" />
-        <Key name="R" filePath="/assets/piano/B4.mp3" audioCtx={audioCtx} keyCode="KeyR" />
-        <Key name="T" filePath="/assets/piano/C5.mp3" audioCtx={audioCtx} keyCode="KeyT" />
-        <Key name="Y" filePath="/assets/piano/D5.mp3" audioCtx={audioCtx} keyCode="KeyY" />
-        <Key name="U" filePath="/assets/piano/E5.mp3" audioCtx={audioCtx} keyCode="KeyU" />
-        <Key name="I" filePath="/assets/piano/F5.mp3" audioCtx={audioCtx} keyCode="KeyI" />
-        <Key name="O" filePath="/assets/piano/G5.mp3" audioCtx={audioCtx} keyCode="KeyO" />
-        <Key name="P" filePath="/assets/piano/A5.mp3" audioCtx={audioCtx} keyCode="KeyP" />
-        <Key name="[" filePath="/assets/piano/B5.mp3" audioCtx={audioCtx} keyCode="BracketLeft" />
-        <Key name="]" filePath="/assets/piano/C6.mp3" audioCtx={audioCtx} keyCode="BracketRight" />
+        {whiteKeys.map((key) => (
+          <Key
+            key={key.keyCode}
+            name={key.name}
+            filePath={key.filePath}
+            audioCtx={audioCtx}
+            keyCode={key.keyCode}
+            isBlack={key.isBlack}
+            left={key.left}
+          />
+        ))}
         <BlackRow>
-          <Key name="2" filePath="/assets/piano/Gb4.mp3" audioCtx={audioCtx} keyCode="Digit2" isBlack left={30.5} />
-          <Key name="3" filePath="/assets/piano/Ab4.mp3" audioCtx={audioCtx} keyCode="Digit3" isBlack left={87.5} />
-          <Key name="4" filePath="/assets/piano/Bb4.mp3" audioCtx={audioCtx} keyCode="Digit4" isBlack left={144.5} />
-          <Key name="6" filePath="/assets/piano/Db5.mp3" audioCtx={audioCtx} keyCode="Digit6" isBlack left={258.5} />
-          <Key name="7" filePath="/assets/piano/Eb5.mp3" audioCtx={audioCtx} keyCode="Digit7" isBlack left={315.5} />
-          <Key name="9" filePath="/assets/piano/Gb5.mp3" audioCtx={audioCtx} keyCode="Digit9" isBlack left={429.5} />
-          <Key name="0" filePath="/assets/piano/Ab5.mp3" audioCtx={audioCtx} keyCode="Digit0" isBlack left={486.5} />
-          <Key name="-" filePath="/assets/piano/Bb5.mp3" audioCtx={audioCtx} keyCode="Minus" isBlack left={543.5} />
+          {blackKeys.map((key) => (
+            <Key
+              key={key.keyCode}
+              name={key.name}
+              filePath={key.filePath}
+              audioCtx={audioCtx}
+              keyCode={key.keyCode}
+              isBlack={key.isBlack}
+              left={key.left}
+            />
+          ))}
         </BlackRow>
       </Row>
       <Row>
-        <Key name="Z" filePath="/assets/piano/C3.mp3" audioCtx={audioCtx} keyCode="KeyZ" />
-        <Key name="X" filePath="/assets/piano/D3.mp3" audioCtx={audioCtx} keyCode="KeyX" />
-        <Key name="C" filePath="/assets/piano/E3.mp3" audioCtx={audioCtx} keyCode="KeyC" />
-        <Key name="V" filePath="/assets/piano/F3.mp3" audioCtx={audioCtx} keyCode="KeyV" />
-        <Key name="B" filePath="/assets/piano/G3.mp3" audioCtx={audioCtx} keyCode="KeyB" />
-        <Key name="N" filePath="/assets/piano/A3.mp3" audioCtx={audioCtx} keyCode="KeyN" />
-        <Key name="M" filePath="/assets/piano/B3.mp3" audioCtx={audioCtx} keyCode="KeyM" />
-        <Key name="," filePath="/assets/piano/C4.mp3" audioCtx={audioCtx} keyCode="Comma" />
-        <Key name="." filePath="/assets/piano/D4.mp3" audioCtx={audioCtx} keyCode="Period" />
-        <Key name="/" filePath="/assets/piano/E4.mp3" audioCtx={audioCtx} keyCode="Slash" />
+        {whiteKeys2.map((key) => (
+          <Key
+            key={key.keyCode}
+            name={key.name}
+            filePath={key.filePath}
+            audioCtx={audioCtx}
+            keyCode={key.keyCode}
+            isBlack={key.isBlack}
+            left={key.left}
+          />
+        ))}
         <BlackRow>
-          <Key name="S" filePath="/assets/piano/Db3.mp3" audioCtx={audioCtx} keyCode="KeyS" isBlack left={30.5} />
-          <Key name="D" filePath="/assets/piano/Eb3.mp3" audioCtx={audioCtx} keyCode="KeyD" isBlack left={87.5} />
-          <Key name="G" filePath="/assets/piano/Gb3.mp3" audioCtx={audioCtx} keyCode="KeyG" isBlack left={201.5} />
-          <Key name="H" filePath="/assets/piano/Ab3.mp3" audioCtx={audioCtx} keyCode="KeyH" isBlack left={258.5} />
-          <Key name="J" filePath="/assets/piano/Bb3.mp3" audioCtx={audioCtx} keyCode="KeyJ" isBlack left={315.5} />
-          <Key name="L" filePath="/assets/piano/Db4.mp3" audioCtx={audioCtx} keyCode="KeyL" isBlack left={429.5} />
-          <Key name=";" filePath="/assets/piano/Eb4.mp3" audioCtx={audioCtx} keyCode="Semicolon" isBlack left={486.5} />
+          {blackKeys2.map((key) => (
+            <Key
+              key={key.keyCode}
+              name={key.name}
+              filePath={key.filePath}
+              audioCtx={audioCtx}
+              keyCode={key.keyCode}
+              isBlack={key.isBlack}
+              left={key.left}
+            />
+          ))}
         </BlackRow>
       </Row>
     </Container>
